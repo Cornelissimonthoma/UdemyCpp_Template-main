29adca70568c7721079d2fc7e7f189edd8f983df
Cornelis Simon-Thomas <cornelis.simon.thomas@t-online.de>
2022-06-13 18:33:34 +0200

    Commit

--- 01_Basics/Main.cc
+++ 01_Basics/Main.cc
@@ -1,9 +1,24 @@
 #include <iostream>
 
 
+unsigned long long factorial(unsigned int n);
+{
+    if (n > 0)
+    {
+        return n * factorial(n);
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+
 int main()
 {
+    unsigned long long n;
 
+    std::cout << "n! = " << factorial(n);
 
     return 0;
 }
